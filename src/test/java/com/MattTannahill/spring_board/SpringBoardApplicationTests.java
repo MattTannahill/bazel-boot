134c76401613b6a61d4aff465c7b2427fa675a53
MattTannahill <matthew.r.tannahill@live.com>
2020-10-15 12:35:17 -0400

    Added Spring Boot project created using initializer (https://start.spring.io/)

---
+++ src/test/java/com/MattTannahill/spring_board/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MattTannahill.spring_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
